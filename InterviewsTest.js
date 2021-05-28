d028bb52c9bbeb123e9fb206490596a2cc1a6f8f
VMO <vmo@Victors-Mac-mini.local>
2021-05-28 14:21:31 +0100

    InterviewTest2 added

--- InterviewsTest.js
+++ InterviewsTest.js
@@ -2,8 +2,9 @@
 // https://www.youtube.com/channel/UC4FHiPgS1KXkUMx3dxBUtPg
 // My own scheme of solution
 
-// 1.- Multiply two number without using "*" --------------------------------
+console.log(`%c TEST #1`, 'font-weight:bold; font-size:24px;');
 console.log('1.--------------');
+// 1.- Multiply two number without using "*" --------------------------------
 // DATA:
 const a = 50;
 const b = 50;
@@ -76,8 +77,11 @@ const expectedResult3 = [1, 2, 3];
 console.log('Expected: ', expectedResult3);
 
 // CODE:
+// Because I am using arrays, I put an empty array [] like second argument
+// "acc" have a initialValue = 0)
 const clean = (arr) =>
 	arr.reduce((acc, val) => {
+		// If val is true, it is different from undefined, null or 0, do that
 		if (val) {
 			acc.push(val);
 		}
@@ -119,3 +123,108 @@ JSON.stringify(expectedResult4) == JSON.stringify(flattenResult)
 	: console.log(`%c TEST FAIL`, 'color:red; font-weight:bold;');
 
 console.log('5.--------------');
+
+// .- Flatten past array another level ----------------------------------------------------------
+
+// DATA:
+console.log('Data: ', flattenResult);
+
+// EXPECTED:
+const expectedResult5 = [1, 2, 3, 4, 1];
+console.log('Expected: ', expectedResult5);
+
+// Using the same function above
+const flattenResult2 = flatten(flattenResult);
+console.log('Result: ', flattenResult2);
+
+// TEST:
+JSON.stringify(expectedResult5) == JSON.stringify(flattenResult2)
+	? console.log(`%c TEST PASS`, 'color:green; font-weight:bold;')
+	: console.log(`%c TEST FAIL`, 'color:red; font-weight:bold;');
+
+console.log('6.--------------');
+
+// 6.- Write a function that counts a most repeated word in array --------------------
+
+// DATA:
+const arrayData6 = 'This is a repeated word counter test, this is a big a';
+console.log('Data: ', arrayData6);
+
+// EXPECTED:
+const expectedResult6 = ['a', 3];
+console.log('Expected: a:', expectedResult6);
+
+// CODE:
+// pass array to lowercase:
+const lowered = arrayData6.toLocaleLowerCase();
+console.log('Step1 lowered: ', lowered);
+
+// split array, make an array with words separated by space:
+const splitted = lowered.split(' ');
+console.log('Step2 splitted: ', splitted);
+
+const repeated = (src) => {
+	// Create an objetc with key= "word" and value= "repetition counter"
+	const wordsObject = {};
+	src.forEach((word) => {
+		wordsObject[word] = !wordsObject[word] ? 1 : (wordsObject[word] += 1);
+	});
+
+	return wordsObject;
+};
+
+const wordsObject = repeated(splitted);
+console.log('Step3 wordsObject: ', wordsObject);
+
+// Objet to Array
+const wordsArray = Object.entries(wordsObject);
+console.log('Step4 wordsArray: ', wordsArray);
+
+// Select the most occurance word
+const wordResult = wordsArray.reduce((acc, val) =>
+	acc[1] > val[1] ? acc : val
+);
+console.log('Result', wordResult);
+
+// TEST:
+JSON.stringify(expectedResult6) == JSON.stringify(wordResult)
+	? console.log(`%c TEST PASS`, 'color:green; font-weight:bold;')
+	: console.log(`%c TEST FAIL`, 'color:red; font-weight:bold;');
+
+console.log('7.--------------');
+// 7.- Palindrome: Verify a word, phrase, or sequence that reads the same backwards as forwards
+
+// DATA:
+const arrayData7 = 'Do geese see God';
+const arrayData7b = 'palabras que no son palindromos';
+console.log('Data: ', arrayData7);
+
+// EXPECTED:
+const expectedResult7 = true;
+console.log('Expected:', expectedResult7);
+
+// CODE:
+// replace or remove spaces
+const replaceTXT = arrayData7.replace(/\s/g, '');
+console.log('Step1 remove space: ', replaceTXT);
+
+// toLowerCase
+const lowerTXT = replaceTXT.toLowerCase();
+console.log('Step2 toLowerCase: ', lowerTXT);
+
+// split array, make an array:
+const splittedTXT = lowerTXT.split('');
+console.log('Step3 toArray: ', splittedTXT);
+
+// reverse order of the array
+const reverseTXT = splittedTXT.reverse();
+console.log('Step4 reverse: ', reverseTXT);
+
+// join the text
+const joinTXT = reverseTXT.join('');
+console.log('Step5 join: ', joinTXT);
+
+// TEST:
+lowerTXT == joinTXT
+	? console.log(`%c TEST PASS`, 'color:green; font-weight:bold;')
+	: console.log(`%c TEST FAIL`, 'color:red; font-weight:bold;');
---
+++ InterviewsTest2.js
@@ -0,0 +1,99 @@
+// Interviews based on the Carlos Azaustre youtube channel
+// https://www.youtube.com/channel/UCJgGc8pQO1lv04VXrBxA_Hg
+
+console.log(`%c TEST #2`, 'font-weight:bold; font-size:24px;');
+console.log('1.--------------');
+// 1.- Create an object with a "hello" method that write "Hello <name>" in the console
+
+const myObject = {
+	name: 'Victor',
+	hello: () => {
+		console.log('Hello', myObject.name);
+	},
+};
+
+myObject.hello();
+
+console.log('2.--------------');
+// 2.- How would you make the <name> inmutable?
+// Using Object.freeze(obj);
+
+// TEST:
+// Changing the object <name> value
+myObject.name = 'Alex';
+myObject.hello();
+
+// CODE:
+Object.freeze(myObject);
+myObject.name = 'Pedro';
+myObject.hello();
+
+console.log('3.--------------');
+// 3.- Write a function that logs the 5 cities that occur the most in the array below
+// in order from the most number of occurrences to least.
+
+const citiesList = [
+	'nashville',
+	'nashville',
+	'los angeles',
+	'nashville',
+	'memphis',
+	'barcelona',
+	'los angeles',
+	'sevilla',
+	'madrid',
+	'canary islands',
+	'barcelona',
+	'madrid',
+	'nashville',
+	'barcelona',
+	'london',
+	'berlin',
+	'madrid',
+	'nashville',
+	'london',
+	'madrid',
+];
+
+// CODE:
+
+function logMostOcurrCities() {
+	// Create an objetc with key= "city name" and value= "repetition counter"
+	// city name = cities[city]
+	// city value => If city name don't exist in a new array "cities" put "1"
+	// else plus "+" 1
+	const cities = {};
+	citiesList.forEach((city) => {
+		cities[city] = !cities[city] ? 1 : (cities[city] += 1);
+	});
+
+	console.log('cites Object: ', cities);
+
+	// Change the object to array, to can sorted it: Object.keys(cities);
+	// And use map function to include the occurance inside the array
+
+	return Object.keys(cities).map((city) => ({
+		name: city,
+		times: cities[city],
+	}));
+}
+
+const citiesArray = logMostOcurrCities();
+console.log('cities Array: ', citiesArray);
+
+// array.sort() method converts the element types into strings and then sorts them
+// Descendent order: array.sort((a, b) => (b - a))
+// This method change the original array
+// Sorted by times:
+const citiesArraySorted = citiesArray.sort((a, b) => b.times - a.times);
+console.log('Array of cities ordered: ', citiesArraySorted);
+
+// Only five cites:
+// .slice(start, end)
+console.log('Array of 5 cities ordered: ', citiesArraySorted.slice(0, 5));
+
+// Only 5 cities name ordered
+console.log(
+	'Array of 5 cities names ordered: ',
+	citiesArraySorted.slice(0, 5).map((city) => city.name)
+);
